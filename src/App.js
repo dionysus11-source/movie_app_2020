1c15b415de886a2d7fe63abf316250c49a2dbeed
dionysus11 <dionysus11@naver.com>
2020-06-20 17:14:05 +0900

    4장 props 공부

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function Food({ name , picture }){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+     <h2>I like {name}</h2>
+     <img src={picture} alt={name} />
+     </div>
+     );
 }
 
+const foodLike = [
+  {id : 1,
+    name : 'Kimchi',
+image :'https://m.jnmall.kr/web/product/big/201910/4b83072de272a51edffa420ab3b2fa98.jpg'},
+{id : 2,
+  name:'Samgyoepsal',
+image : 'https://lh3.googleusercontent.com/proxy/VUvgYtXlK1kuQNT_uiGqhuQZ4NXc96B-QELYk2ebgfxSMjjJJYEtkZvo2RkvexABHl7jzPHQtHwsMbBaC9dig0d5C5L_ZSv0nYKfQaPsgs9PCZh7f8B2FBbFUbZRJmupoMRBSUmrXw' },
+{id : 3,
+  name:'Kimbap',
+image : 'https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg' }
+];
+
+
+function App() {
+  //console.log(foodLike.map(renderFood));
+  return (
+  <div>
+    <h1>안녕!!</h1>
+    {foodLike.map(dish=>(
+    <Food key = {dish.id} name={dish.name} picture={dish.image} />)
+    )}
+  </div>
+  );
+}
 export default App;
